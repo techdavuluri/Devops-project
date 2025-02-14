036e4613a9dabd401669ab4a5680184e051362fa
Praveen <tech.davuluri@gmail.com>
2025-02-13 18:13:49 -0800

    Adding Microservices

---
+++ bookstore-frontend/src/main/java/acme/frontend/ServicePaths.java
@@ -0,0 +1,16 @@
+package acme.frontend;
+
+/**
+ * Holds some commonly used paths.
+ */
+final class ServicePaths {
+
+  private ServicePaths() { }
+
+  //static String DEBUG_URL = "http://localhost:8002/application/";
+  static String DEBUG_URL = "http://bookstore-devservice:8002/application/";
+
+
+  // static String DATA_MANAGER_URL = "http://localhost:8001/";
+  static String DATA_MANAGER_URL = "http://bookstore-datamanager:8001/";
+}
